8068a8cfdeaf19dff4535e71b2e44021de37454d
sp1d3o <valenta1313@seznam.cz>
2023-01-13 02:35:34 +0100

    Designed singleton patter + dir finding function

--- Log.h
+++ Log.h
@@ -1,42 +1,44 @@
 #ifndef LOGGER_LOG_H
 #define LOGGER_LOG_H
 
-//standard headers
+#ifdef WINDOWS
+#include <direct.h>
+#define GetCurrentDir _getcwd
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
+
 #include <iostream>
-#include <fstream>
-#include <filesystem>
+#include <mutex>
 
-//custom headers
-#include "Error.h"
+/*
+std::string get_current_dir()
+{
+    char buffer[FILENAME_MAX];
+    GetCurrentDir(buffer, FILENAME_MAX);
+    std::string current_working_dir(buffer);
+    return current_working_dir;
+}
+*/
 
-class Logger {
-private:
-    const char *m_message;
-    std::ofstream f_stream;
-
-protected:
-    Logger() = default;
-
+ class Logger {
+ protected:
+    Logger(const std::string value) : p_value(value) {}
+    static Logger * p_logger;
+    std::string p_value;
 public:
-    //Handling deleted functions + destructor
     Logger(const Logger &) = delete;
-    Logger(const Logger &&) = delete;
-    Logger &operator=(const Logger &) = delete;
-    Logger &operator=(const Logger&&) = delete;
-    ~Logger() = default;
-    //function to create singleton
-    static Logger & get_object()
-    {
-        static Logger logger;
-        return logger;
+    void operator=(const Logger &) = delete;
+
+    static Logger * GetInstance(const std::string & value);
+    std::string value() const {
+        return p_value;
     }
-
-    friend Logger &operator<<(Logger & logger , const char *message);
-    bool FileExist(const std::filesystem::path &p, std::filesystem::file_status s = std::filesystem::file_status{});
-    std::fstream &CreateFile(const std::string & name);
-
 };
 
-Logger & logger = Logger::get_object();
+Logger * Logger::p_logger = nullptr;
+
+
 
 #endif //LOGGER_LOG_H
